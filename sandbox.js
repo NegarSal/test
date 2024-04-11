e921ecd80167e79fcab366915e2d859fe986baae
Negar <negarsaljooghi@gmail.com>
2024-04-11 11:30:15 +0330

    callback function

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,12 @@
 const ul = document.querySelector('.people');
 
 const people = ['Mario', 'Luigi', 'Ryu', 'Shaun', 'Chun-li'];
+
+let html = ``;
+
+people.forEach(function(person){
+    // create html template
+    html += `<li style='color: purple'>${person}</li>`
+})
+
+ul.innerHTML = html;
